f9042d253f78914392c0eacfe0e20d05fb8ac50f
“heber-tolosagit config --global user.email heber_darkanes@hotmail.com <heber_darkanes@hotmail.com>
2025-04-25 12:24:05 -0300

    first commit

---
+++ src/components/Form/Form.tsx
@@ -0,0 +1,52 @@
+import { ChangeEvent, useState } from "react";
+import { countries } from "../../data/countries";
+import styles from "./Form.module.css";
+import type { SearchType } from "../../types";
+
+export default function Form() {
+  const [search, setSearch] = useState<SearchType>({
+    city: "",
+    country: "",
+  });
+  const handleChange = (
+    e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>
+  ) => {
+    setSearch({
+      ...search,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  return (
+    <form className={styles.form}>
+      <div className={styles.field}>
+        <label htmlFor="city">Ciudad:</label>
+        <input
+          type="text"
+          name="city"
+          id="city"
+          placeholder="Ciudad"
+          value={search.city}
+          onChange={handleChange}
+        />
+      </div>
+      <div className={styles.field}>
+        <label htmlFor="country">Pais:</label>
+        <select
+          name="country"
+          id="country"
+          value={search.country}
+          onChange={handleChange}
+        >
+          <option value="">-- Seleccione un pais --</option>
+          {countries.map((country) => (
+            <option key={country.code} value={country.code}>
+              {country.name}
+            </option>
+          ))}
+        </select>
+      </div>
+      <input type="submit" className={styles.submit} value="Consultar clima" />
+    </form>
+  );
+}
