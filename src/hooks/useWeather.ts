08b794bcc5eb86daa08287bd12d6b88e65f2427e
heber tolosa <heber_darkanes@hotmail.com>
2025-04-27 15:56:03 -0300

    add zod and valibot schemas

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -1,5 +1,28 @@
 import axios from "axios";
+import { z } from "zod";
+import { object, string, number, InferOutput, parse } from "valibot";
 import { SearchType } from "../types";
+
+/* const Weather = z.object({
+  name: z.string(),
+  main: z.object({
+    temp: z.number(),
+    temp_max: z.number(),
+    temp_min: z.number(),
+  }),
+});
+type Weather = z.infer<typeof Weather>;
+*/
+const WeatherSchema = object({
+  name: string(),
+  main: object({
+    temp: number(),
+    temp_max: number(),
+    temp_min: number(),
+  }),
+});
+
+type Weather = InferOutput<typeof WeatherSchema>;
 
 export default function useWeather() {
   const fetchWeather = async (search: SearchType) => {
@@ -10,8 +33,13 @@ export default function useWeather() {
       const lat = data[0].lat;
       const lon = data[0].lon;
       const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiID}`;
+      /* 
+      ZOD
       const { data: weatherResult } = await axios(weatherUrl);
-      console.log(weatherResult);
+      const result = Weather.safeParse(weatherResult); */
+      const { data: weatherResult } = await axios(weatherUrl);
+      const result = parse(WeatherSchema, weatherResult);
+      console.log(result);
     } catch (error) {
       console.log(error);
     }
